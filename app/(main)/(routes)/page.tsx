e12e35bc0c7b83d7a2245922c0091ccf32c62032
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-10-29 06:03:02 +0800

    setup theme

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
-import { UserButton } from "@clerk/nextjs";
+import { ModeToggle } from '@/components/mode-toggle';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
