b6fa13ccff2af02cfadfd4316bcfe3e0419f9c8f
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-12-27 01:02:23 +0800

    create server settings functionalities

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -3,6 +3,7 @@
 import React, { useEffect, useState } from 'react'
 import CreateServerModal from '@/components/modals/create-server-modal';
 import InviteModal from '@/components/modals/invite-modal';
+import EditServerModal from '@/components/modals/edit-server-modal';
 
 export default function ModalProvider() {
   const [isMounted, setIsMounted] = useState(false);
@@ -19,6 +20,7 @@ export default function ModalProvider() {
     <>
       <CreateServerModal />
       <InviteModal />
+      <EditServerModal />
     </>
   )
 }
