de6df7a60c722411e7c639e2915eabe0c74cc8da
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-12-30 15:21:13 +0800

    set redirect functions to channel id page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+import { redirectToSignIn } from "@clerk/nextjs";
 
-export default function ServerIdPage() {
-  return (
-    <div>ServerPage</div>
-  )
+interface ServerIdPageProps {
+  params: {
+    serverId: string;
+  };
+}
+
+export default async function ServerIdPage({ params }: ServerIdPageProps) {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params?.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+
+  const initialChannel = server?.channels?.[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(
+    `/servers/${params?.serverId}/channels/${initialChannel?.id}`
+  );
 }
