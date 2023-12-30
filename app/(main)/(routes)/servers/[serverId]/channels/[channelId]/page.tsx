2397350b94af0854445efa7b1ef06ce84a501239
Antoni Luthfi <luthfiantoni027@gmail.com>
2023-12-30 22:19:32 +0800

    create chat input functionalities

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,4 +1,5 @@
 import ChatHeader from "@/components/chat/chat-header";
+import ChatInput from "@/components/chat/chat-input";
 import React from "react";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
@@ -42,6 +43,16 @@ export default async function ChannelIdPage({ params }: ChannelIdPageProps) {
         name={channel.name}
         serverId={channel.serverId}
         type="channel"
+      />
+      <div className="flex-1">Future Message</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{ 
+          channelId: channel.id, 
+          serverId: channel.serverId 
+        }}
       />
     </div>
   );
